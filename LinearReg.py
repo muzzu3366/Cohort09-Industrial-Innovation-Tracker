95d96e150830db14062d8cd86078912057eab641
rgd2004 <153278291+rgd2004@users.noreply.github.com>
2026-03-03 11:51:50 +0530

    Add files via upload

---
+++ LinearReg.py
@@ -0,0 +1,12 @@
+def train_model_lr(X, y):
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    return model, X_test, y_test, y_pred
